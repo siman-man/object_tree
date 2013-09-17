321c545297f0563b09f3c25b1c1925a9cf578fce
siman-man <k128585@ie.u-ryukyu.ac.jp>
2013-09-18 03:11:25 +0900

    little fix

--- example/sample.rb
+++ example/sample.rb
@@ -1,10 +1,6 @@
 require 'object_tree'
 
-tree = ObjectTree::Tree.new(Numeric)
-obj_list = tree.object_list
-obj_list = tree.object_tree(obj_list)
-tree.draw(obj_list)
-
-tree.module_proc
-obj_list =  tree.object_list_module
-tree.draw(obj_list)
+p tree = ObjectTree::Tree.create(BasicObject, true)
+#obj_list = tree.object_list
+#obj_list = tree.object_tree(obj_list)
+tree.draw
